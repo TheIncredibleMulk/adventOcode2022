9f7ed30c1112a110e3e08ee05588cf0547ee5fe2
Mulk <59551118+TheIncredibleMulk@users.noreply.github.com>
2022-12-13 05:01:30 +0000

    1/2 way through day 2

--- day 2/day2.go
+++ day 2/day2.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+type round struct {
+}
+
+var decoder = make(map[string]string)
+
+var rounds [][]string
+
+func main() {
+	fmt.Println("advent of code 2022 day 2")
+
+	readFile, err := os.Open("day2_puzzle_input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	// create map of codes to wins
+	decoder["a"] = "rock"
+	decoder["x"] = "rock"
+	decoder["b"] = "paper"
+	decoder["y"] = "paper"
+	decoder["c"] = "scissor"
+	decoder["z"] = "scissor"
+
+	for fileScanner.Scan() {
+		moves := strings.Split(fileScanner.Text(), " ")
+		rounds = append(rounds, moves)
+	}
+
+	readFile.Close()
+
+	for _, v := range rounds {
+		//  Compare each round and Figure out if we won or lost.
+		fmt.Println(v)
+	}
 }
