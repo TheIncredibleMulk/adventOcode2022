50f6d27265d55002b34f96b880ff4d10567ed515
Mulk <59551118+TheIncredibleMulk@users.noreply.github.com>
2022-12-15 04:51:43 +0000

    says its the wrong answer but I feel like this is correct. :/

--- day 2/day2.go
+++ day 2/day2.go
@@ -7,16 +7,23 @@ import (
 	"strings"
 )
 
-type round struct {
+type winningStat struct {
+	num    int
+	kind   string
+	points int
 }
 
 var decoder = make(map[string]string)
 
 var rounds [][]string
 
+var stats []winningStat
+
 func main() {
+
 	fmt.Println("advent of code 2022 day 2")
 
+	// readFile, err := os.Open("day2_sample_input.txt")
 	readFile, err := os.Open("day2_puzzle_input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -26,12 +33,21 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	// create map of codes to wins
-	decoder["a"] = "rock"
-	decoder["x"] = "rock"
-	decoder["b"] = "paper"
-	decoder["y"] = "paper"
-	decoder["c"] = "scissor"
-	decoder["z"] = "scissor"
+	decoder["A"] = "rock"
+	decoder["X"] = "rock"
+	decoder["B"] = "paper"
+	decoder["Y"] = "paper"
+	decoder["C"] = "scissor"
+	decoder["Z"] = "scissor"
+	
+	// point values for wins
+	drawPoints := 3
+	winPoints := 6
+	lossPoints := 0
+	rockPoints := 1
+	paperPoints := 2
+	scissorPoints := 3
+	var points int
 
 	for fileScanner.Scan() {
 		moves := strings.Split(fileScanner.Text(), " ")
@@ -40,8 +56,51 @@ func main() {
 
 	readFile.Close()
 
-	for _, v := range rounds {
+	points = 0
+	for i, v := range rounds {
 		//  Compare each round and Figure out if we won or lost.
-		fmt.Println(v)
+		opponent := decoder[v[0]]
+		you := decoder[v[1]]
+		var stat winningStat
+		stat.num = i
+		stat.kind = you
+
+		switch you {
+		case "rock":
+			switch opponent {
+			case "rock":
+				stat.points = drawPoints + rockPoints
+			case "paper":
+				stat.points = lossPoints + rockPoints
+			case "scissor":
+				stat.points = winPoints + rockPoints
+
+			}
+		case "paper":
+			switch opponent {
+			case "rock":
+				stat.points = winPoints + paperPoints
+			case "paper":
+				stat.points = drawPoints + paperPoints
+			case "scissor":
+				stat.points = lossPoints + paperPoints
+			}
+		case "scissor":
+			switch opponent {
+			case "rock":
+				stat.points = lossPoints + rockPoints
+			case "paper":
+				stat.points = winPoints + scissorPoints
+			case "scissor":
+				stat.points = drawPoints + scissorPoints
+			}
+
+		}
+		stats = append(stats, stat)
+		points = points + stat.points
 	}
+	// for _, v := range stats {
+	// 	points = v.points + points
+	// }
+	fmt.Printf("%+v\n", points)
 }
